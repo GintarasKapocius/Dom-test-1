d6c4f7695095ca25fcf3d4124a13e1eb3de3ae9c
GintarasKapocius <gintaraskapocius@gmail.com>
2021-11-13 23:11:20 +0200

    task 5

---
+++ js/API.js
@@ -0,0 +1,19 @@
+const baseUrl = 'http://localhost:3000'
+
+class API {
+  static fetchCars = (success, failure) => {
+    fetch(`${baseUrl}/cars`)
+      .then((res) => res.json())
+      .then(success)
+      .catch(failure)
+  }
+
+  static deleteCar = (id, success, failure) => {
+    fetch(`${baseUrl}/cars/${id}`, { method: 'DELETE' })
+      .then((res) => (res.ok ? success() : failure(res.statusText)))
+      .catch(failure)
+  }
+}
+
+// API.fetchCars(console.log, console.error)
+// API.deleteCar('2', () => console.log('Item deleted'), console.error)
